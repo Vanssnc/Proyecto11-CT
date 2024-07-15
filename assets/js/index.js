183c3ddd3019d0d983ffb054d9d735380def2c39
Vanessa Nicole <vanessa.ncore@gmail.com>
2024-07-14 23:56:39 -0400

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+let precio = 400000;
+const precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+let cantidad = document.querySelector(".cantidad");
+let precioBase = document.querySelector(".precio-inicial");
+let valorTotal = document.querySelector(".valor-total");
+
+const suma = document.querySelector(".suma");
+const resta = document.querySelector(".resta");
